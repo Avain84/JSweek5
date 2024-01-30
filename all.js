cd019b0329f9c33d313152a8cf1a4aee19646ecb
Avain <sports113499@gmail.com>
2024-01-30 15:17:15 +0800

    新增篩選功能

--- all.js
+++ all.js
@@ -30,3 +30,125 @@ let data = [
     "rate": 7
   }
 ];
+// DOM
+// 表單區
+const ticketName = document.querySelector("#ticketName");
+const ticketImgUrl = document.querySelector("#ticketImgUrl");
+const ticketRegion = document.querySelector("#ticketRegion");
+const ticketPrice = document.querySelector("#ticketPrice");
+const ticketNum = document.querySelector("#ticketNum");
+const ticketRate = document.querySelector("#ticketRate");
+const ticketDescription = document.querySelector("#ticketDescription");
+const addTicketbtn = document.querySelector(".addTicket-btn");
+// 搜尋區
+const regionSearch = document.querySelector(".regionSearch");
+const searchResultText = document.querySelector("#searchResult-text");
+// 套票卡片區
+const ticketCardarea = document.querySelector(".ticketCard-area");
+
+
+// 將資料呈現在畫面
+function showCardData(dataArr){
+  let cardTxt = '';
+  // 頁面card內容利用innerHTML更改
+  dataArr.forEach(function(item){
+    cardTxt += `
+      <li class="ticketCard">
+        <div class="ticketCard-img">
+          <a href="#">
+            <img src=${item.imgUrl} alt="">
+          </a>
+          <div class="ticketCard-region">${item.area}</div>
+          <div class="ticketCard-rank">${item.rate}</div>
+        </div>
+        <div class="ticketCard-content">
+          <div>
+            <h3>
+              <a href="#" class="ticketCard-name">${item.name}</a>
+            </h3>
+            <p class="ticketCard-description">
+              ${item.description}
+            </p>
+          </div>
+          <div class="ticketCard-info">
+            <p class="ticketCard-num">
+              <span><i class="fas fa-exclamation-circle"></i></span>
+              剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
+            </p>
+            <p class="ticketCard-price">
+              TWD <span id="ticketCard-price">${item.price}</span>
+            </p>
+          </div>
+        </div>
+      </li> 
+    `;
+  });
+  // 將組好的結構放到ul內
+  ticketCardarea.innerHTML = cardTxt;
+}
+
+// 資料寫入、呈現、更動篩選出來的數量
+function showAndChange(arr){
+  // 新增資料同時新增下方卡片
+  showCardData(arr);
+  // 隨著資料變動內容
+  searchResultText.textContent = `本次搜尋共 ${arr.length} 筆資料`;
+}
+
+// 初始資料
+showCardData(data);
+
+// 新增資料按鈕監聽
+addTicketbtn.addEventListener("click",e => {
+  let addData = {
+    "id": data.length,
+    "name": ticketName.value,
+    "imgUrl": ticketImgUrl.value,
+    "area": ticketRegion.value,
+    "description": ticketDescription.value,
+    "group": ticketNum.value,
+    "price": ticketPrice.value,
+    "rate": ticketRate.value
+  };
+  
+  // 要全部都有資料才會 push
+  if(addData.name !== "" && addData.imgUrl !== "" && addData.area !== "" && addData.description !== "" && addData.group !== "" && addData.price !== "" && addData.rate !== ""){
+    if(addData.rate > 10 || addData.rate < 1){
+      alert(`套票星級區間為 1-10 分`);
+      return ;
+    }
+    if(addData.description.length > 100 ){
+      // console.log(addData.description.length);
+      alert(`套票描述不得超過100字`);
+      return ;
+    }
+    data.push(addData);
+  }else{
+    alert("請確認是否所有資料都有正確輸入");
+  }
+  showAndChange(data);
+  ticketName.value = null;
+  ticketImgUrl.value = null;
+  ticketRegion.value = "";
+  ticketPrice.value = null;
+  ticketNum.value = null;
+  ticketRate.value = null;
+  ticketDescription.value = null;
+});
+
+// 搜尋區篩選 
+regionSearch.addEventListener("change", e => {
+  if(e.target.value == ""){
+    showAndChange(data);
+    return ;
+  }
+
+  let ticketRegionSelect = [];
+
+  data.forEach(function(item){
+    if(e.target.value === item.area){
+      ticketRegionSelect.push(item);
+    }
+  });
+  showAndChange(ticketRegionSelect);
+});
